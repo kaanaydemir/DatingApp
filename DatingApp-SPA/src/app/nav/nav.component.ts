c1acab6c3556e7834cd2560b17a2e846777e119a
Kaan Aydemir <kaan.aydemir.iu@gmail.com>
2019-03-10 12:41:37 +0300

    End of section 4

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -21,5 +21,13 @@ export class NavComponent implements OnInit {
       console.log('Failed to login');
     });
   }
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+  logout(){
+    localStorage.removeItem('token');
+    console.log('Logged Out');
+  }
 
 }
